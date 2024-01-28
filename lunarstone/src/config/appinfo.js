0cac7a5f23e1f7d383b3239fe35cc4c2d6f8c802
fanxiaolong <developer@lunarhook.com>
2024-01-29 01:09:06 +0800

    u

---
+++ lunarstone/src/config/appinfo.js
@@ -0,0 +1,13 @@
+
+import React, {Component} from 'react';
+
+var appinfo = {
+'lunarstone': 'wx88d194568817e0cc',
+'lunarhook': 'wxae6d3fb9b27ce6f6',
+}
+var appname = {
+'lunarstone': '星罗坊',
+'lunarhook': '月如钩',
+}
+
+module.exports={appinfo,appname};  
